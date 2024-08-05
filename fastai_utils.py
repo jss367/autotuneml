d1d0b0a5169924ed2cea67b9c6192e501c5d4124
Julius Simonelli <julius.simonelli@gmail.com>
2024-08-04 23:25:36 -0700

    Update fastai_utils.py

--- fastai_utils.py
+++ fastai_utils.py
@@ -73,7 +73,6 @@ def prepare_fastai_data(df: pd.DataFrame, target: str, problem_type: str):
 def fastai_objective(trial, data, problem_type):
     # Define the hyperparameters to optimize
     layers = trial.suggest_categorical('layers', [[200, 100], [500, 200], [1000, 500, 200]])
-    emb_drop = trial.suggest_float('emb_drop', 0, 0.5)
     ps = trial.suggest_float('ps', 0, 0.5)
     bs = trial.suggest_categorical('bs', [32, 64, 128, 256])
 
@@ -81,8 +80,9 @@ def fastai_objective(trial, data, problem_type):
     dls = data.dataloaders(bs=bs)
 
     # Create and train the model
+    config = tabular_config(ps=ps, embed_p=trial.suggest_float('embed_p', 0, 0.5))
     learn = tabular_learner(
-        dls, layers=layers, emb_drop=emb_drop, ps=ps, metrics=accuracy if problem_type == 'classification' else rmse
+        dls, layers=layers, config=config, metrics=accuracy if problem_type == 'classification' else rmse
     )
     learn.fit_one_cycle(5)
 
@@ -98,6 +98,7 @@ def fastai_objective(trial, data, problem_type):
 
 
 def train_fastai_with_optuna(data, problem_type, n_trials=50):
+    logger.info("Starting FastAI training with Optuna")
     study = optuna.create_study(direction='minimize')
     study.optimize(lambda trial: fastai_objective(trial, data, problem_type), n_trials=n_trials)
 
@@ -106,12 +107,9 @@ def train_fastai_with_optuna(data, problem_type, n_trials=50):
 
     # Train the final model with the best parameters
     dls = data.dataloaders(bs=best_params['bs'])
+    config = tabular_config(ps=best_params['ps'], embed_p=best_params['embed_p'])
     learn = tabular_learner(
-        dls,
-        layers=best_params['layers'],
-        emb_drop=best_params['emb_drop'],
-        ps=best_params['ps'],
-        metrics=accuracy if problem_type == 'classification' else rmse,
+        dls, layers=best_params['layers'], config=config, metrics=accuracy if problem_type == 'classification' else rmse
     )
     learn.fit_one_cycle(5)
 
