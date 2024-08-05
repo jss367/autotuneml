5a3e0ec61db8c901f3772661286372ba55397cf7
Julius Simonelli <julius.simonelli@gmail.com>
2024-08-05 14:18:38 -0700

    Update log_config.py

--- log_config.py
+++ log_config.py
@@ -1,9 +1,34 @@
 import logging
 
+# ANSI escape codes for colors
+RED = '\033[91m'
+RESET = '\033[0m'
+
+
+class ColoredFormatter(logging.Formatter):
+    def format(self, record):
+        if record.levelno == logging.ERROR:
+            record.msg = f"{RED}{record.msg}{RESET}"
+        return super().format(record)
+
 
 def setup_logger():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # Create console handler and set level to INFO
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    # Create formatter
+    formatter = ColoredFormatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # Add formatter to ch
+    ch.setFormatter(formatter)
+
+    # Add ch to logger
+    logger.addHandler(ch)
+
     return logger
 
 
